b1c63c2b5b1413f9a20bf5ea486a312a2aa3278c
计算机界的bug <61082556+zjw-coder@users.noreply.github.com>
2020-11-14 15:05:04 +0800

    Update map.py

--- map.py
+++ map.py
@@ -1,3 +1,8 @@
 def normalize(name):
     name=name[0].upper()+name[1:].lower()
     return name
+#测试
+if __name__ == '__main__':
+    L1 = ['adam', 'LISA', 'barT']
+    L2 = list(map(normalize, L1))
+    print(L2)
